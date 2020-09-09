e51c69677c44ee24a2070ae2868102131e97b3ab
Matthew Kindt <matthewkindt@yahoo.com>
2020-09-09 01:41:03 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def using_include(array, element)
 end
 
 def using_sort(array = ["wow", "I", "am", "really", "learning", "arrays!"])
-  array.sort("wow", "I")
+  array.sort
 end
 
 def using_reverse(array)
